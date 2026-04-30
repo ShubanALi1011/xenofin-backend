bb7c9a649fe802226c5bda4d975dc5fb1074b7b4
Shuban Ali <shubanali1011@gmail.com>
2026-04-30 18:51:27 +0500

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,16 +1,14 @@
 <?php
-// Railway ke environment variables read karne ke liye getenv use karen
 $host = getenv('MYSQLHOST');
 $user = getenv('MYSQLUSER');
 $pass = getenv('MYSQLPASSWORD');
 $db   = getenv('MYSQL_DATABASE'); 
 $port = getenv('MYSQLPORT');
 
-// Connection string
+// Connection string with explicit variables
 $conn = mysqli_connect($host, $user, $pass, $db, $port);
 
 if (!$conn) {
-    // Sirf debug ke liye error show karega
     die("Connection Failed: " . mysqli_connect_error());
 }
 ?>
